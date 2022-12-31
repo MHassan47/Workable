1a0f86465b0cda8b45882d118e0823e373c87505
Mohamed <mhassankd35@gmail.com>
2022-12-30 20:04:01 -0600

    connects server to postgres DB

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,11 +2,24 @@ import "reflect-metadata";
 import { ApolloServer } from "apollo-server-express";
 import express, { Express, Request, Response } from "express";
 import { buildSchema } from "type-graphql";
-
+import { createConnection } from "typeorm";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
-import { UserResolver } from "./resolvers/user";
+
+import { User } from "./entities/User";
+import { UserResolver } from "./resolvers/UserResolver";
 
 const main = async () => {
+  const conn = await createConnection({
+    type: "postgres",
+    database: "workable",
+    entities: [User],
+    logging: true,
+    synchronize: true,
+    username: "postgres",
+    password: "postgres",
+    port: 5432,
+  });
+
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [UserResolver],
@@ -22,7 +35,7 @@ const main = async () => {
   apolloServer.applyMiddleware({ app });
 
   app.get("/", (_req: Request, res: Response) => res.send("Hello World"));
-  const PORT = 3000;
+  const PORT = 5000;
 
   app.listen(PORT, () => console.log(`Server running on ${PORT}`));
 };
