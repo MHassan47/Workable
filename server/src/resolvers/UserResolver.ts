13989f41a45eb9c425824d4e32c7602cd67af0ee
Mohamed <mhassankd35@gmail.com>
2022-12-31 02:37:21 -0600

    adds context and middleware to protected resolvers

--- server/src/resolvers/UserResolver.ts
+++ server/src/resolvers/UserResolver.ts
@@ -1,17 +1,24 @@
 import { User } from "../entities/User";
 import {
   Arg,
+  Ctx,
   Field,
+  Int,
   Mutation,
   ObjectType,
   Query,
   Resolver,
+  UseMiddleware,
 } from "type-graphql";
 import bcrypt from "bcryptjs";
 import { sign } from "jsonwebtoken";
+import { MyContext } from "src/MyContext";
+import { isAuth } from "../middleware/isAuth";
 
 @ObjectType()
 class LoginResponse {
+  @Field(() => Int)
+  user: number;
   @Field()
   accessToken: string;
 }
@@ -36,20 +43,31 @@ export class UserResolver {
     return true;
   }
 
-  @Mutation(() => Boolean)
-  async login(@Arg("email") email: string, @Arg("password") password: string) {
+  @Mutation(() => LoginResponse)
+  async login(
+    @Arg("email") email: string,
+    @Arg("password") password: string
+  ): Promise<LoginResponse> {
     const user = await User.findOne({ where: { email } });
-
+    console.log(user);
     if (!user) {
       throw new Error("could not find user");
     }
-    const validPassword = bcrypt.compare(password, user.password);
+    const validPassword = await bcrypt.compare(password, user.password);
+    console.log(validPassword);
     if (!validPassword) {
       throw new Error("password incorrect");
     }
 
     return {
+      user: user.id,
       accessToken: sign({ userId: user.id }, "secretkey", { expiresIn: "15m" }),
     };
+  }
+
+  @Query(() => String)
+  @UseMiddleware(isAuth)
+  bye(@Ctx() { payload }: MyContext) {
+    return `user id: ${payload?.userId}`;
   }
 }
