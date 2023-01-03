7996d27680881f30045c80c2ace7f1dd2f17571c
Mohamed <mhassankd35@gmail.com>
2023-01-03 17:29:01 -0600

    creates initial app routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,13 @@
+import Login from "./components/Login";
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <div>Hello</div>
+      <Routes>
+        {/* <Route path="/" element={<Landing />} /> */}
+        <Route path="/login" element={<Login />} />
+        {/* <Route path="/transaction" element={<Transaction />}  */}
+      </Routes>
     </div>
   );
 }
