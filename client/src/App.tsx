4674e079f3e24111185acb1a3fce1e70a721465d
Mohamed <mhassankd35@gmail.com>
2023-01-03 22:43:31 -0600

    imports and creates register route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
-import Login from "./components/Login";
 import { Route, Routes } from "react-router-dom";
+import Login from "../src/components/Auth/Login";
+import Register from "../src/components/Auth/Register";
 function App() {
   return (
     <div className="App">
       <Routes>
         {/* <Route path="/" element={<Landing />} /> */}
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         {/* <Route path="/transaction" element={<Transaction />}  */}
       </Routes>
     </div>
