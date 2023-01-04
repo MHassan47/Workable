62bbaafb4680f4018d0c02cb5891a713c75ca7df
Mohamed <mhassankd35@gmail.com>
2023-01-03 22:45:28 -0600

    creates login and create user mutation logic

---
+++ client/src/GraphQL/Mutations.ts
@@ -0,0 +1,26 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      user
+      accessToken
+    }
+  }
+`;
+
+export const REGISTER = gql`
+  mutation createUser(
+    $firstName: String!
+    $lastName: String!
+    $email: String!
+    $password: String!
+  ) {
+    createUser(
+      firstName: $firstName
+      lastName: $lastName
+      email: $email
+      password: $password
+    )
+  }
+`;
