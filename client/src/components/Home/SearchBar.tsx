efd0a97d81e813159cc3cad705f5093e238f94ff
Mohamed <mhassankd35@gmail.com>
2023-01-13 11:26:29 -0600

    refactors search logic to modify query rather than filter results

--- client/src/components/Home/SearchBar.tsx
+++ client/src/components/Home/SearchBar.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { useLazyQuery } from "@apollo/client";
+import { SEARCHJOBS } from "../../GraphQL/Queries";
 
 interface Job {
   id: number;
@@ -16,46 +18,36 @@ interface Props {
 }
 
 const SearchBar: React.FC<Props> = ({ jobs, setJobs }) => {
-  // State to store the search term
   const [searchTerm, setSearchTerm] = useState("");
+  const [searchJobs, { error, data, loading }] = useLazyQuery(SEARCHJOBS, {
+    onCompleted: (data) => {
+      if (data.searchJobs) setJobs(data.searchJobs);
+      //   console.log(data.searchJobs);
+    },
+  });
 
-  // Function to handle changes to the search input
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchTerm(e.target.value);
-  };
-
-  //   const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
-  //     if (e.key === "Enter") {
-  //       handleSearch(e);
-  //     }
-  //   };
-
-  // Function to filter the jobs based on the search term
   const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const filteredJobs = jobs.filter((job) => {
-      return job.title.toLowerCase().includes(searchTerm.toLowerCase());
-    });
-    setJobs(filteredJobs);
+    searchJobs({ variables: { searchTerm } });
   };
 
   return (
     <div className="w-2/3 mb-6">
-      <div className="flex flex-row justify-around border-2 p-2 rounded-xl">
+      <div className="flex flex-row border-2 p-2 rounded-xl">
         <form onSubmit={handleSearch}>
           <input
             type="text"
-            placeholder="Search for jobs..."
+            placeholder="Search for job title, company or location..."
             value={searchTerm}
-            onChange={handleChange}
-            className="outline-none"
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className="outline-none "
           />
           <button type="submit">Search</button>
         </form>
       </div>
-      <div className="ml-2 mt-2">
+      {/* <div className="ml-2 mt-2">
         {jobs.length} {jobs && jobs.length > 1 ? "results" : "result"}{" "}
-      </div>
+      </div> */}
     </div>
   );
 };
