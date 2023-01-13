24b4359953eb143c1fa44b57a60aafdf2af350f8
Mohamed <mhassankd35@gmail.com>
2023-01-13 13:55:49 -0600

    fixes apollo client authorization header

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
 import App from "./App";
 import "./index.css";
-const client = new ApolloClient({
+import { setContext } from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({
   uri: "http://localhost:5000/graphql",
-  cache: new InMemoryCache(),
 });
 
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem("authorization");
+  console.log(token);
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
