d23c71d2d723d43bb35f553222e2e58e19e93202
Mohamed <mhassankd35@gmail.com>
2023-01-13 14:26:17 -0600

    creates initial sidebar component

---
+++ client/src/components/Home/Sidebar.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Sidebar() {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>Search Jobs</li>
+        <li>Top Companies</li>
+        <li>Post Job</li>
+        <li>Saved Jobs</li>
+        <li>Settings</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Sidebar;
