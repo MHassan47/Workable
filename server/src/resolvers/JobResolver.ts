11a65e3e6a0ab254524b710303f1ef2093caf278
Mohamed <mhassankd35@gmail.com>
2023-01-13 12:38:39 -0600

    creates search query that filters job list by title, location or company

--- server/src/resolvers/JobResolver.ts
+++ server/src/resolvers/JobResolver.ts
@@ -10,17 +10,21 @@ import {
   Resolver,
   UseMiddleware,
 } from "type-graphql";
+import { dataSource } from "../dataSource";
+import { User } from "../entities/User";
 
 @Resolver()
 export class JobResolver {
   //   @UseMiddleware(isAuth)
   @Query(() => [Job])
   async allJobs(): Promise<Job[]> {
-    return Job.find({});
+    return Job.find();
   }
 
   @Mutation(() => Boolean)
+  @UseMiddleware(isAuth)
   async createJob(
+    @Ctx() { payload }: MyContext,
     @Arg("company") company: string,
     @Arg("title") title: string,
     @Arg("description") description: string,
@@ -28,7 +32,7 @@ export class JobResolver {
     @Arg("salaryMaximum", () => Int) salaryMaximum: number,
     @Arg("location") location: string
   ) {
-    await Job.insert({
+    const newJob = await Job.save({
       company,
       title,
       description,
@@ -36,12 +40,29 @@ export class JobResolver {
       salaryMaximum,
       location,
     });
+
+    await dataSource
+      .createQueryBuilder()
+      .relation(User, "myJob")
+      .of(payload?.userId)
+      .add(newJob);
     return true;
   }
 
-  //   @Query(() => [Job])
-  //   @UseMiddleware(isAuth)
-  //   async savedJobs(@Ctx() { payload }: MyContext): Promise <Jobs[]>{
-  //     return await Job.find({where: })
-  //   }
+  @Query(() => [Job])
+  async searchJobs(@Arg("searchTerm") searchTerm: string): Promise<Job[]> {
+    const jobs = await Job.createQueryBuilder("job")
+      .where(
+        "LOWER(job.title) LIKE :searchTerm OR LOWER(job.company) LIKE :searchTerm OR LOWER(job.location) LIKE :searchTerm",
+        { searchTerm: `%${searchTerm}%` }
+      )
+      .getMany();
+    return jobs;
+  }
+
+  // @Query(() => [Job])
+  // @UseMiddleware(isAuth)
+  // async savedJobs(@Ctx() { payload }: MyContext): Promise <Jobs[]>{
+  //   return await Job.find({where: })
+  // }
 }
