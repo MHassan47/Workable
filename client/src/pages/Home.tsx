a87ab5dbb7611a6e1ef8a46906d69c733e0bf543
Mohamed <mhassankd35@gmail.com>
2023-01-13 06:43:47 -0600

    makes changes to state type

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,13 +3,14 @@ import { useQuery } from "@apollo/client";
 import { GET_ALL_JOBS } from "../GraphQL/Queries";
 import JobDetails from "../components/Home/JobDetails";
 import JobPostings from "../components/Home/JobPostings";
+import SearchBar from "../components/Home/SearchBar";
 
 function Home() {
   const { error, loading, data } = useQuery(GET_ALL_JOBS, {
     onCompleted: (data) => {},
   });
 
-  const [jobs, setJobs] = useState([]);
+  const [jobs, setJobs] = useState<Job[]>([]);
   const [selected, setSelected] = useState<Job | null>(null);
 
   interface Job {
@@ -31,14 +32,15 @@ function Home() {
   console.log(selected);
   return (
     <div className="flex justify-center h-screen">
-      <div className="flex w-1/2 justify-center">
+      <div className="flex flex-col w-1/2 justify-center">
+        <SearchBar jobs={jobs} setJobs={setJobs} />
         <JobPostings
           jobs={jobs}
           selected={selected}
           setSelected={setSelected}
         />
       </div>
-      <div className="flex w-1/3">
+      <div className="flex w-1/3 items-center">
         {selected && <JobDetails job={selected} />}
       </div>
     </div>
