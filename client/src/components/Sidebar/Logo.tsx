c70070aaedcda4e09a31ecdf45ea112faed5e86c
Mohamed <mhassankd35@gmail.com>
2023-01-14 01:45:51 -0600

    creates simple app logo

---
+++ client/src/components/Sidebar/Logo.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Logo() {
+  return (
+    <header className="text-3xl text-center font-bold text-blue-600">
+      Workable
+    </header>
+  );
+}
+
+export default Logo;
