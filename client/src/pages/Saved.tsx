39df04d93ea8b1bf71ca12e235cc8637e2fc5bcc
Mohamed <mhassankd35@gmail.com>
2023-01-14 11:06:44 -0600

    imports job detail component in saved jobs page

--- client/src/pages/Saved.tsx
+++ client/src/pages/Saved.tsx
@@ -1,11 +1,12 @@
 import { useQuery } from "@apollo/client";
 import React, { useEffect, useState } from "react";
+import JobDetails from "../components/Home/JobDetails";
 import JobPostings from "../components/Home/JobPostings";
 
 import Sidebar from "../components/Sidebar/Sidebar";
 import { GET_SAVED_JOBS } from "../GraphQL/Queries";
 
-interface Job {
+export interface Job {
   id: number;
   company: string;
   title: string;
@@ -24,19 +25,22 @@ function Saved() {
 
   useEffect(() => {
     if (data) setSavedJobs(data.getAllSavedJobs);
-  }, [data]);
+  }, []);
 
   return (
     <div className="flex justify-center h-screen">
       <Sidebar />
       <div className="flex flex-col items-center w-1/2 justify-center">
+        <h1 className="font-bold text-3xl mb-6">Saved Jobs</h1>
         <JobPostings
           jobs={savedJobs}
           selected={selected}
           setSelected={setSelected}
         />
       </div>
-      <div className="flex w-1/3 items-center"></div>
+      <div className="flex w-1/3 items-center">
+        {selected && <JobDetails job={selected} />}
+      </div>
     </div>
   );
 }
