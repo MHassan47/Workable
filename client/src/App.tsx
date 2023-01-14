c761dafde1bd6111d85092c3533e4b83880f8f79
Mohamed <mhassankd35@gmail.com>
2023-01-14 11:02:32 -0600

    adds private routing to protected pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Login from "../src/components/Auth/Login";
 import Register from "../src/components/Auth/Register";
-import JobPostings from "./components/Home/JobPostings";
+import PrivateRoutes from "./components/Other/PrivateRoute";
 import CreateJob from "./pages/CreateJob";
 import Home from "./pages/Home";
 import Saved from "./pages/Saved";
@@ -12,9 +12,11 @@ function App() {
         {/* <Route path="/" element={<Landing />} /> */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/saved" element={<Saved />} />
-        <Route path="/create" element={<CreateJob />} />
+        <Route element={<PrivateRoutes />}>
+          <Route path="/home" element={<Home />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/create" element={<CreateJob />} />
+        </Route>
       </Routes>
     </div>
   );
