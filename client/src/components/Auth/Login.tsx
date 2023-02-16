e1e0c3a787f6e5cec68516371b9a70ee5e065634
Mohamed <mhassankd35@gmail.com>
2023-02-16 06:00:19 -0600

    imports landing left component login page

--- client/src/components/Auth/Login.tsx
+++ client/src/components/Auth/Login.tsx
@@ -2,6 +2,7 @@ import { useMutation, useQuery } from "@apollo/client";
 import { FC, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { LOGIN } from "../../GraphQL/Mutations";
+import LandingLeft from "./LandingLeft";
 function Login() {
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
@@ -29,38 +30,41 @@ function Login() {
     }
   };
   return (
-    <div className=" flex flex-col justify-center items-center  w-4/6  h-screen">
-      <h1 className="text-3xl font-bold text-sky-400 text-left mb-12">
-        Sign In
-      </h1>
-      <input
-        className="  w-2/5 mb-10 p-2 rounded-lg border-2 border-black"
-        type="email"
-        name="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        required={true}
-        placeholder="Email"
-      />
-      <input
-        className="  w-2/5 mb-10 p-2 rounded-lg border-2 border-black"
-        type="password"
-        name="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        required={true}
-        placeholder="Password"
-      />
+    <div className="flex flex-row w-full">
+      <LandingLeft />
+      <div className=" flex flex-col justify-center items-center  w-3/5  h-screen">
+        <h1 className="text-3xl font-bold text-sky-400 text-left mb-12">
+          Sign In
+        </h1>
+        <input
+          className="  w-2/5 mb-10 p-2 rounded-lg border-2 border-black"
+          type="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required={true}
+          placeholder="Email"
+        />
+        <input
+          className="  w-2/5 mb-10 p-2 rounded-lg border-2 border-black"
+          type="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required={true}
+          placeholder="Password"
+        />
 
-      <a className=" flex w-2/5 text-center" href="/login">
-        Forgot your password?
-      </a>
-      <button
-        onClick={onSubmitHandler}
-        className="bg-sky-600 h-10 w-20 rounded-lg mt-10"
-      >
-        Sign In
-      </button>
+        <a className=" flex w-2/5 text-center" href="/login">
+          Forgot your password?
+        </a>
+        <button
+          onClick={onSubmitHandler}
+          className="bg-sky-600 h-10 w-20 rounded-lg mt-10"
+        >
+          Sign In
+        </button>
+      </div>
     </div>
   );
 }
